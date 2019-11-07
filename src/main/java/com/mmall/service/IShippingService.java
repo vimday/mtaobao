74d9976a9a95165dff13830fdf434a4e1035895d
Neo <lwfplus@gmail.com>
2019-11-07 12:17:54 +0800

    Add files via upload

---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,20 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+public interface IShippingService {
+
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId,Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+
+    ServerResponse<Shipping> select(Integer id, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer id, int pageNum, int pageSize);
+}
